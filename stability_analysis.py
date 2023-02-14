c38664840c6c5fd725dd43767a9067daad48e86c
Poornima1304 <108811601+Poornima1304@users.noreply.github.com>
2023-02-14 20:19:46 +0100

    add comments

--- stability_analysis.py
+++ stability_analysis.py
@@ -1,3 +1,4 @@
+# Python script to collect data from MATLAB/SIMULINK model of Exoskeleton and pharse data to decide the stability of exoskeleton motion
 import time
 import os
 import numpy as np
@@ -7,12 +8,9 @@ import matplotlib.pyplot as plt
 import csv
 import platform
 import socket, struct
-old_ts_length = 11
-threshold = 0.4
-prior_idx = 1
 
 
-
+# Recieve UDP Packets from MATLAB
 udp_socket2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_socket2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
 udp_socket2.bind(("127.0.0.1", 6337))
@@ -25,25 +23,49 @@ y_list = []
 time_s =[]
 print('INIT COMPLETE')
 print('WAITING FOR SAFELEGS')
-
 start = time.time()
-for count in range(0,101,1): 
+
+
+
+
+
+for count in range(0,101,1):
+  # recive data and unpack it and transform it to a array for further processing
   recv_msg2 = recv_data2[0]
   send_addr2 = recv_data2[1] 
+  
   recv_msg_decode = struct.unpack("dddddddddddddddddddddddddddd", recv_msg2)
   recv_msg_decode = np.asarray(recv_msg_decode)
+  
+  # preparing Index
   x_idx = list(range(0,12))
   z_idx = list(range(14,26))
   com_idx = [12,26]
+  
+  # Transform data
   x = np.asarray(recv_msg_decode[x_idx])
   x = x[~np.isnan(x)]
   z = np.asarray(recv_msg_decode[z_idx])
   z = z[~np.isnan(z)]
+  
+  # save center of mass of the exoskeleton
   com = list(recv_msg_decode[com_idx])
   time_sim= recv_msg_decode[13]
+  
+  # save Base of Support points the points represents the feet location points and "X" represents center of mass
+  #          ._______________________________.
+  #        .|                                  |.
+  #          |.                              .| 
+  #          |                               |
+  #          |            X                  | 
+  #          |.                               |.
+  #        .|                                  |.
+  #          .________________________________.
+  # the about figure represents the support polygon and COM
+  
+  
   bos= np.column_stack((x,z))
   hull=ConvexHull(points=bos)  
-        
   pointstodraw=[]
   for n in hull.vertices:         # Has the index of the choosen vertices from list 'p'
     pointstodraw.append(bos[n])   # We are adding all the vertices corresponding to the index number of p in a new list pointstodraw
@@ -60,16 +82,20 @@ for count in range(0,101,1):
   
 
   if poly_path.contains_point(com):
+    # if COM is inside the BOS the model is stable
     a = "YES"
     y_list.append(a)
     time_s.append(time_sim)
     print("Yes: ",time_sim)
   else:
+    # if not model is unstable
     a = "NO"
     y_list.append(a)
     time_s.append(time_sim)
     print("No: ",time_sim)
 
+    
+# save the stabiliy data into scv for further ML training
 header = ["Time","Labels"]    
 with open('Labels.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.DictWriter(f, fieldnames=header)
